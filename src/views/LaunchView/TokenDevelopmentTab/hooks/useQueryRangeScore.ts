1e65cc2128cb2866bc5f3d2c049cea267bbb843c
alan <dvdunguet@gmail.com>
2024-11-08 00:52:10 +0700

    disable when user not connect

--- src/views/LaunchView/TokenDevelopmentTab/hooks/useQueryRangeScore.ts
+++ src/views/LaunchView/TokenDevelopmentTab/hooks/useQueryRangeScore.ts
@@ -1,9 +1,12 @@
 import { useBaseQuery } from '@/hooks/useBaseQuery';
+import useWalletActive from '@/hooks/useWalletActive';
 import { getTotalUserWithScore } from '@/services/api';
 
 export const useQueryTotalUserWithScore = (min_target_score: number, max_target_score: number) => {
+  const { address } = useWalletActive();
   return useBaseQuery({
-    queryKey: ['getTotalUserWithScore', min_target_score, max_target_score],
+    queryKey: ['getTotalUserWithScore', min_target_score, max_target_score, address],
     queryFn: () => getTotalUserWithScore({ min_target_score, max_target_score }),
+    enabled: !!address,
   });
 };
